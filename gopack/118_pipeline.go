68cf08b22a263988496b5fbe3b50b80ead405442
Camden <camdenc520@gmail.com>
2021-11-30 13:58:04 -0800

    Add 1.18 pipeline

---
+++ gopack/118_pipeline.go
@@ -0,0 +1,24 @@
+package gopack
+
+func Make118Pipeline() {
+	pipeline := CreatePipeline("to 1.18 (remove secrets, flattening)", TransPath("work/118/"), "1.18.zip")
+
+	// update format
+	pipeline.AddForFileName("pack.mcmeta", SetMetaRevision(8))
+	// remove comments
+	pipeline.AddForFileType("json", RemoveComment())
+
+	MakeEverythingLowercase(pipeline)
+	RenamePackFolders(pipeline)
+	ConvertItems(pipeline, Get14Blocks())
+	ConvertItems(pipeline, Get14Items())
+	ConvertItems(pipeline, GetBlocks())
+	ConvertItems(pipeline, GetItems())
+	ForceContent(pipeline, GetForcedContent())
+	MigrateLanguage(pipeline, GetLang())
+
+	CompressResources(pipeline)
+
+	pipeline.SaveUntouched()
+	AddPipeline(pipeline)
+}
